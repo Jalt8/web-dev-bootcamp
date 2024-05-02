18a6a4774806d2944737588194f3c1b2f1759e42
Juvan Holder <juvanh@mip.co.za>
2024-05-03 00:41:25 +0200

    -add to section38: clean up select

--- section38/index.js
+++ section38/index.js
@@ -18,6 +18,8 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+const categories = ["fruit", "vegetable", "dairy"]
+
 app.get("/products", async (req, res) => {
     const products = await Product.find({})
     console.log(products)
@@ -34,7 +36,7 @@ app.post("/products", async (req, res) => {
 })
 
 app.get("/products/new", (req, res) => {
-    res.render('products/new')
+    res.render('products/new', { categories })
 })
 
 app.get("/products/:id", async (req, res) => {
@@ -48,12 +50,12 @@ app.get("/products/:id/edit", async (req, res) => {
     const { id } = req.params
     const product = await Product.findById(id)
     console.log(product)
-    res.render('products/edit', { product })
+    res.render('products/edit', { product, categories })
 })
 
 app.put("/products/:id", async (req, res) => {
     const { id } = req.params
-    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
     res.redirect(`/products/${product.id}`)
 })
 
