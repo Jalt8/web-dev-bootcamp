77015c06f902679efa17dd6d3d87da173d6a5e31
Juvan Holder <juvanh@mip.co.za>
2024-05-07 23:19:13 +0200

    - add initial validations to YelpCamp

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
+const AppError = require('./utils/AppError');
+const wrapAsync = require('./utils/catchasync')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .then(() => {
@@ -24,45 +26,78 @@ app.listen(3000, () => {
     console.log("RUNNING ON PORT 3000!")
 })
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', wrapAsync(async (req, res) => {
     const campgrounds = await Campground.find()
     res.render("campgrounds/index.ejs", { campgrounds })
-})
+}))
 
-app.get('/campgrounds/new', async (req, res) => {
+app.get('/campgrounds/new', wrapAsync(async (req, res) => {
     res.render("campgrounds/new.ejs")
-})
+}))
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', wrapAsync(async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findById(id)
+    if (!campground) {
+        return next(new AppError('Campground Not Found', 404));
+    }
     res.render('campgrounds/show.ejs', { campground })
+}))
 
-})
-
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', wrapAsync(async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findById(id)
+    if (!campground) {
+        return next(new AppError('Campground Not Found', 404));
+    }
     res.render('campgrounds/edit.ejs', { campground })
+}))
 
-})
-
-app.post('/campgrounds', async (req, res) => {
-    console.log(req.body)
+app.post('/campgrounds', wrapAsync(async (req, res) => {
     const newCampground = new Campground(req.body)
     await newCampground.save()
+    if (!req.body) {
+        return next(new AppError('No request body', 404));
+    }
     res.redirect(`campgrounds/${newCampground.id}`)
-})
+}))
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', wrapAsync(async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+    if (!campground) {
+        return next(new AppError('Campground Not Found', 404));
+    }
     res.redirect(`/campgrounds/${campground.id}`)
-})
+}))
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', wrapAsync(async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findByIdAndDelete(id)
+    if (!campground) {
+        return next(new AppError('Campground Not Found', 404));
+    }
     res.redirect(`/campgrounds`)
+}))
+
+const handleCastErr = err => {
+    console.dir(err);
+    //In a real app, we would do a lot more here...
+    return new AppError(`Cast Failed...${err.message}. Please provide a valid number`, 400)
+}
+
+app.use((err, req, res, next) => {
+    console.log(err.name);
+    //We can single out particular types of Mongoose Errors:
+    if (err.name === 'CastError') err = handleCastErr(err)
+    next(err);
 })
 
+app.all(('*'), (req, res, next) => {
+    next(new AppError('Page Not Found'), 404)
+})
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = 'Something Went Wrong' } = err
+    res.status(status).send(message)
+})
